4d537d496fd573c303d7e698d50891d3206d6653
mateuszgil <mateusz.gil@ready4s.com>
2023-05-02 12:22:42 +0200

    fix  failing full row test

--- src/lib.rs
+++ src/lib.rs
@@ -2,39 +2,30 @@ pub fn up(board: &mut[ [i32; 4]; 4]) {
     let mut occupied_spots = [0; 16];
     for i in 0..board.len() {
         for j in 0..board[i].len() {
-
-            
             if board[i][j] != 0 {
                 let spot = j + i * 4 ;
-                if i == 0  {
-                    occupied_spots[spot] = 1;
-                    continue;
-                }
-                
+                occupied_spots[spot] = 1;
                 let mut top_free_spot = j;
-
-                while occupied_spots[top_free_spot] != 1 {
+                while occupied_spots[top_free_spot] == 1 {
+                    
                     top_free_spot += 4;
-                    if top_free_spot > 15 {
+                    if top_free_spot >= spot {
                         break;
                     }
                 }
-
-                if top_free_spot > 15  {
+            
+                if top_free_spot >= spot {
                     occupied_spots[spot] = 1;
                     continue;
                 }
-
                 let top_row = top_free_spot / 4;
-                println!("{}", top_row);
                 board[top_row][j] = board[i][j];
                 board[i][j] = 0;
                 occupied_spots[spot] = 0;
-               
+                occupied_spots[top_free_spot] = 1;
             }
         }
     }
-    println!("{:?}", occupied_spots);
 }
 
 pub fn down() {
